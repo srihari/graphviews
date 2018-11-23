37a66edbe76ad94c25ad54a197cc8c0a6cbd102c
srihari <1nf0rmix>
2018-11-23 19:15:29 +0530

    Graph DB Stuff

---
+++ src/main/java/com/example/graphview/GraphViewApplication.java
@@ -0,0 +1,12 @@
+package com.example.graphview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphViewApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/graphview/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.graphview.controllers;
+
+import org.janusgraph.core.JanusGraph;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    private static final String url = "";
+    private JanusGraph airroutes;
+
+
+    @RequestMapping("/airroutes/{airportCode}")
+    public ModelAndView viewCustomer(ModelAndView modelAndView, @PathVariable String airportCode) {
+        connect();
+
+        modelAndView.setViewName("airroutes");
+
+        return modelAndView;
+    }
+
+    private void connect() {
+
+
+    }
+
+
+}
