8380f433511ab877b42530e7a9536b60922d8813
Rahul <rahulna@thoughtworks.com>
2018-11-24 20:48:16 +0530

    Submit gremlin command to local janusgraph

--- src/main/java/com/example/graphview/controllers/HomeController.java
+++ src/main/java/com/example/graphview/controllers/HomeController.java
@@ -1,10 +1,17 @@
 package com.example.graphview.controllers;
 
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.Result;
 import org.janusgraph.core.JanusGraph;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Controller
 public class HomeController {
@@ -15,15 +22,24 @@ public class HomeController {
     @RequestMapping("/airroutes/{airportCode}")
     public ModelAndView viewRoutes(ModelAndView modelAndView, @PathVariable String airportCode) {
         connect();
-
         modelAndView.setViewName("airroutes");
 
         return modelAndView;
     }
 
     private void connect() {
+        Cluster cluster = Cluster.open();   //connects to the localhost when no config is passed
+        Client client = cluster.connect();
 
-
+        CompletableFuture<List<Result>> results = client.submit("1+1").all();
+        try {
+            Result shouldBe2 = results.get().get(0);
+            System.out.println(shouldBe2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
 
