5e7fdf502179412e14610ca742cd4afdc9181400
Rahul <rahulna@thoughtworks.com>
2018-12-09 00:55:53 +0530

    Rahul | Open existing remote graph WIP

--- src/main/java/com/example/graphview/controllers/HomeController.java
+++ src/main/java/com/example/graphview/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.example.graphview.controllers;
 
 import com.example.graphview.graph.RemoteGraph;
 import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,10 @@ public class HomeController {
     }
 
     private void executeQuery(String airportCode) throws Exception {
-        remoteGraph.openGraph(JANUS_CONF_FILE_PATH);
+        GraphTraversalSource graphTraversalSource = remoteGraph.openGraph(JANUS_CONF_FILE_PATH);
+        String city = (String) graphTraversalSource.V().limit(1).values("city").next();
+        System.out.println("Here is your >>>>>>>> " + city);
+
         Result result = remoteGraph.executeGremlinCommand(traversalScript(airportCode), new HashMap<>());
         remoteGraph.closeGraph();
     }
